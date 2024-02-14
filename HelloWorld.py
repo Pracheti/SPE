32ac0e890d8745ec8d059f0cde56f918d83e9578
Pracheti <33132237+Pracheti@users.noreply.github.com>
2024-02-14 10:22:01 +0530

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,2 +1,6 @@
-print("Hello World");
-
+#! /usr/bin/python3
+# This Phython program will print Hellow World...
+print("Hello World Demo...Jan 2024...\n")
+print("Hello World Demo...Jan 2024...\n")
+print("Hello World Demo...Jan 2024...\n")
+print("Hello World Demo...Jan 2024...\n")
